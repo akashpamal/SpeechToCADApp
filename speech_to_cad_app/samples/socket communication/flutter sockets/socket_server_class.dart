99858d0fb747b5bd047c2ae497a0009e658b4dfb
Akash <akashpamal@gmail.com>
2021-06-01 14:31:24 -0400

    successful communication between python and flutter using sockets (in samples)

--- speech_to_cad_app/samples/socket communication/flutter sockets/socket_server_class.dart
+++ speech_to_cad_app/samples/socket communication/flutter sockets/socket_server_class.dart
@@ -3,27 +3,11 @@ import 'dart:typed_data';
 import 'dart:core';
 // import
 
-
 void main() async {
   // bind the socket server to an address and port
-  final server = await ServerSocket.bind(InternetAddress.anyIPv4, 4567);
-  var client_variable = null;
-
-  // print('Runtime type:' + server.runtimeType.toString());
-  // print('isBroadcast: ' + server.isBroadcast.toString());
-  // client.write('Hello there');
-  // client.write('This is my second message');
-  // client.write('Welcome to the gulag');
-
-  // listen for client connections to the server
-  server.listen((client) {
-    // client.write('Hello there');
-    handleConnection(client);
-    // client_variable = client;
-  });
+  var socketServerObject = SocketServerClass();
 
   // client_variable.write("Message from client variable");
-
 
   // client.write("Hello there again");
   // server.listen((client) {
@@ -33,46 +17,49 @@ void main() async {
   // server.listen((client) {
   //   client.write('Welcome to the gulag');
   // });
-
-
-
 }
 
-void handleConnection(Socket client) {
-  print('Connection from'
-      ' ${client.remoteAddress.address}:${client.remotePort}');
+class SocketServerClass {
+  var server = null;
 
-  // client.write('Hello there');
-  // client.write('This is my second message');
-  // client.write('Welcome to the gulag');
+  SocketServerClass() {
+    print('Constructor for SocketServerClass');
+    this._establishConnection();
+  }
 
-  client.listen(
+  void _establishConnection() async {
+    final server = await ServerSocket.bind(InternetAddress.anyIPv4, 4567);
+    this.server = server;
 
-    // handle data from the client
-        (Uint8List data) async {
-      // await Future.delayed(Duration(seconds: 1));
-      final message = String.fromCharCodes(data);
-      print('Messaged received: ' + message);
-      // if (message == 'Knock, knock.') {
-      //   client.write('Who is there?');
-      // } else if (message.length < 10) {
-      //   client.write('$message who?');
-      // } else {
-      //   client.write('Very funny.');
-      //   client.close();
-      // }
-    },
+    // listen for client connections to the server
+    server.listen((client) {
+      handleConnection(client);
+    });
+  }
 
-    // handle errors
-    onError: (error) {
-      print(error);
-      client.close();
-    },
+  void handleConnection(Socket client) {
+    print('Connection from'
+        ' ${client.remoteAddress.address}:${client.remotePort}');
 
-    // handle the client closing the connection
-    onDone: () {
-      print('Client left');
-      client.close();
-    },
-  );
+    client.listen(
+      // handle data from the client
+      (Uint8List data) async {
+        // await Future.delayed(Duration(seconds: 1));
+        final message = String.fromCharCodes(data);
+        print('Messaged received: ' + message);
+      },
+
+      // handle errors
+      onError: (error) {
+        print(error);
+        client.close();
+      },
+
+      // handle the client closing the connection
+      onDone: () {
+        print('Client left');
+        client.close();
+      },
+    );
+  }
 }
